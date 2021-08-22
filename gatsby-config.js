8dbc48d91907e2a77bd9596447913da803315a79
Crisdell James Ines <cj.ines@outlook.ph>
2021-08-23 02:28:56 +0800

    rename description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `Pokedex App using Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `A serverless Pokedex Application to learn the JAMStack `,
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
